b7d8eb52cbef50045bd940e7656051b44dd8e803
kyeonghwan.lim <rudghks1007@naver.com>
2024-04-09 17:28:28 +0900

    API 연동에서 막힘

--- __init__.py
+++ __init__.py
@@ -1,11 +1,6 @@
 from ui.ui import *
 ##import ui.ui as eee  ##2번안
 ##from ui.ui import Ui_calss  ##UI 폴더의 UI py 의 Ui_class 가져옴
-
-
-
-
-
 class Main():
     def __init__(self):
         print("실행할 메인 클래스")
@@ -17,8 +12,4 @@ if __name__=="__main__":        ## 이것이 메인 실행파일이다 명시하
     Main()
 
 
-print("testtest")
-
-for i in range(5):
-    print("test")
 
--- ui/ui.py
+++ ui/ui.py
@@ -1,6 +1,17 @@
 from kiwoom.kiwoom import *
+
+import sys                                  ##파이썬 시스템 라이브러리
+
+
+from PyQt5.QtWidgets import *               ##PyQt ui적인거 꾸밀때 사용
+
+
 class Ui_calss():                           ## 클래스의 앞글자는 대문자로
     def __init__(self):
         print("UI_class 입니다")
 
+        self.app = QApplication(sys.argv)   ##sys.argv 리스트형태로 인자들이 들어있음['파이썬파일경로',추가할옵션','---]
+
         Kiwoom()
+
+        self.app.exec_()                    ##증권프로그램 안꺼지게 이벤트루프
